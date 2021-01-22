99061131e140d52a67469368e41b5ae03ed3b463
Hieu Ngo <enginoobz@gmail.com>
2021-01-22 21:45:16 +0100

    UI: Player health label

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Collider), typeof(Rigidbody))]
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField] float immuneDuration = 2;
     [SerializeField] float maxHealth = 10;
+    [SerializeField] TextMeshProUGUI label;
     float currentHealth;
     Collider theCollider;
     // Start is called before the first frame update
@@ -14,6 +16,7 @@ public class HealthSystem : MonoBehaviour
     {
         theCollider = GetComponent<MeshCollider>();
         currentHealth = maxHealth;
+        updateLabel();
     }
 
     // Update is called once per frame
@@ -30,8 +33,15 @@ public class HealthSystem : MonoBehaviour
     IEnumerator getDamaged()
     {
         currentHealth--;
+        updateLabel();
+
         theCollider.enabled = false;
         yield return new WaitForSeconds(immuneDuration);
         theCollider.enabled = true;
+    }
+
+    private void updateLabel()
+    {
+        label.text = "Health " + currentHealth + "/" + maxHealth;
     }
 }
