3b3d00bd088758baed656c4c9ba930ebb8f7a969
Hieu Ngo <enginoobz@gmail.com>
2021-01-24 07:17:02 +0100

    Implement TerrainSpawner to spawn and position set of pre-designed terrain tiles

---
+++ Assets/Scripts/TerrainSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour
+{
+    [SerializeField] public float terrainSize;
+    [SerializeField] Terrain[] tiles;
+
+    int currentCoord = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(TestSpawn), 10f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // coord: 0->256 1->256+512
+    public void SpawnOnCoord(int coord)
+    {
+        tiles[0].transform.position = new Vector3(terrainSize / 2 + coord * terrainSize, 0, -terrainSize / 2);
+    }
+
+    public void TestSpawn()
+    {
+        SpawnOnCoord(++currentCoord);
+    }
+}
